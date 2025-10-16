58fd55a2397df69aa04733b7663de02f64cde24e
TheeLMe <146256246+TheeLMe@users.noreply.github.com>
2025-10-16 12:32:51 -0300

    Implemented code for user to add X or O when a sqaure is clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
   const squares = document.querySelectorAll("#board div");
+  const gameState = Array(9).fill(null);
+  let player = "X"
 
-  squares.forEach(function (square) {
+
+  squares.forEach(function (square, index) {
     square.setAttribute("class", "square");
+
+    square.addEventListener("click", function(){
+        if (gameState[index] != null ) return;
+
+        square.textContent = player;
+        square.classList.add(player);
+
+        gameState[index] = player;
+        player = player == "X" ? "O" : "X";
+        
+    })
   });
 });
